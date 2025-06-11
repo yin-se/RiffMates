ad79fa182cad8c8da43a1941c3fa6a53897d7e75
Yin Wang <yin.dev@outlook.com>
2025-06-11 05:52:14 -0400

    added credit in RiffMates/home/views/py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def credits(request):
+    content = "Nicky\nYin Wang"
+    return HttpResponse(content, content_type="text/plain")
