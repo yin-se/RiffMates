11e5449a653ca27864044b70f10d6039466dc668
Yin Wang <yin.dev@outlook.com>
2025-06-16 22:06:30 -0400

    Page 79; Continue from Chap 4.5

--- bands/models.py
+++ bands/models.py
@@ -5,3 +5,6 @@ class Musician(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     birth = models.DateField()
+
+    def __str__(self):
+        return f"Musician (id={self.id}, last_name = {self.last_name})"
